e8bc888f9f2f115ab2e2492439ac290c03af0c7d
Luke Hayes <lukepurplehayes@gmail.com>
2023-03-29 10:30:16 +0100

    Added window edge bounds checking.

--- src/MoveSystem.c
+++ src/MoveSystem.c
@@ -15,6 +15,23 @@ void MoveSystem(Entity** ents, int count, float dt)
 
         e->drawComponent->y += e->velocityComponent->vy *
                               e->velocityComponent->speed * dt;
+
+        int x = e->drawComponent->x; 
+        int y = e->drawComponent->y; 
+
+        if(x <= 0 || x > 1260)
+        {
+            e->velocityComponent->vx = -e->velocityComponent->vx;
+
+        }
+
+        if(y <= 0 || y > 700)
+        {
+            e->velocityComponent->vy = -e->velocityComponent->vy;
+
+        }
+
+
     }
 
 
