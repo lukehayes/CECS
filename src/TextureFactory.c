e5267af5a54ed36688ea3aaa37793a281fced058
Luke Hayes <lukepurplehayes@gmail.com>
2023-04-03 10:56:52 +0100

    Added image scaling to initTextureFactory().

--- src/TextureFactory.c
+++ src/TextureFactory.c
@@ -1,5 +1,8 @@
 #include "component/util/TextureFactory.h"
 
+#define BASE_IMAGE_TILE_SIZE 16
+#define TILE_SCALE_FACTOR 1
+
 /**
  * Initialize all of the textures in a single location.
  */
@@ -8,7 +11,16 @@ TextureFactory* initTextureFactory()
   LOG("Initializing Texture Factory");
   TextureFactory* factory = malloc(sizeof(TextureFactory));
 
+  // TODO Need to add a better way to scale an image.
   Image spriteImage = LoadImage("../assets/debug_sprite.png");
+
+  // Scale the image from its base size.
+  ImageResize(&spriteImage, 
+      BASE_IMAGE_TILE_SIZE * TILE_SCALE_FACTOR,
+      BASE_IMAGE_TILE_SIZE * TILE_SCALE_FACTOR
+  );
+
+
   factory->debug_sprite = LoadTextureFromImage(spriteImage);
 
   return factory;
