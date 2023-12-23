5f744266d7d00cca7e9318756cf7b34203b462ff
Luke Hayes <lukepurplehayes@gmail.com>
2023-12-23 11:11:47 +0000

    Moved ENTITY_COUNT to ECS/Globals.h

---
+++ include/ECS/Globals.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+
+#define ENTITY_COUNT 100
+#define COMPONENT_COUNT 100
+
+#endif // GLOBALS_H
--- include/Externs.h
+++ include/Externs.h
@@ -1,14 +1,9 @@
 #ifndef CECS_EXTENRS_H
 #define CECS_EXTENRS_H
 
-
 /**
  * All extern variables are defined here in a single location.
  */
-
-//extern int ENTITY_COUNT;
-#define ENTITY_COUNT 10
-
 extern int MAX_TIMER_COUNT;
 
 #endif //CECS_EXTENRS_H
