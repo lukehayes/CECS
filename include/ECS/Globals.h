d327a2b08c3a76f2a015e8160d83110a5823fa72
Luke Hayes <lukepurplehayes@gmail.com>
2023-12-29 10:59:16 +0000

    Added Color constants to ECS/Global.h

--- include/ECS/Globals.h
+++ include/ECS/Globals.h
@@ -1,8 +1,12 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-
 #define ENTITY_COUNT 100
 #define COMPONENT_COUNT 100
+
+#define BG    (Color){76,38,61,255});
+#define DARK  (Color){102,84,81,255}
+#define LIGHT (Color){178,156,152,255}
+#define WHITE (Color){255,250,246, 255}
 
 #endif // GLOBALS_H
