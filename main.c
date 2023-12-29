7468ca56241130fd121c518c38b45dbf28125621
Luke Hayes <lukepurplehayes@gmail.com>
2023-12-29 11:02:11 +0000

    Updated main with latest test code.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "Engine.h"
 #include "ECS/System.h"
 #include "ECS/Component.h"
+#include "ECS/Globals.h"
 
 #include <stdio.h>
 
@@ -11,22 +12,7 @@ int main() {
 
     initEngine();
 
-    ComponentList* components = createComponentList();
-
-    for(int x = 0; x <= COMPONENT_COUNT - 1; x++)
-    {
-        for(int y = 0; y <= COMPONENT_COUNT - 1; y++)
-        {
-            int N = 30;
-            int rx = x;
-            int ry = y;
-            addTransform(x + y + 1, rx,ry, BLUE, components);
-        }
-    }
-
-    /*addTransform(0,100,100, BLUE, components);*/
-    /*addTransform(1,200,500, RED, components);*/
-    /*addTransform(1,200,500, RED, components);*/
+    ComponentList* components = fillComponentList();
 
     while (!WindowShouldClose()) {
 
@@ -34,6 +20,7 @@ int main() {
 
         delta = GetFrameTime();
 
+        UpdateSystem(components, delta);
         DrawSystem(components);
     }
 
