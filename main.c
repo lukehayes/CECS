d7332da414aed571c1b3c825e56945d81845ac32
Luke Hayes <lukepurplehayes@gmail.com>
2023-12-30 11:09:35 +0000

    Added FPS and entity count text for debugging.

--- main.c
+++ main.c
@@ -11,8 +11,10 @@ float delta = 0.0;
 int main() {
 
     initEngine();
-
     ComponentList* components = fillComponentList();
+
+    addTransform(0, 100,100, LIGHT, components);
+    addCollision(0, 100,100, 20, components);
 
     while (!WindowShouldClose()) {
 
@@ -21,7 +23,20 @@ int main() {
         delta = GetFrameTime();
 
         UpdateSystem(components, delta);
+        CollisionSystem(components, delta);
         DrawSystem(components);
+
+        char fps[100];
+        char ent_count[100];
+
+        sprintf(fps, "FPS: %i", GetFPS());
+        sprintf(ent_count, "Entity Count: %i", COMPONENT_COUNT);
+
+        BeginDrawing();
+            DrawText(fps, 10,10, 26, WHITE);
+            DrawText(ent_count, 10,50, 26, WHITE);
+        EndDrawing();
+
     }
 
     destroyComponentList(components);
